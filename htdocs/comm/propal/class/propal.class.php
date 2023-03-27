4fa181a55a85bc9b4fe17177c91491f4cb0d3d9c
Mathieu Moulin <mathieu@iprospective.fr>
2023-03-27 17:45:27 +0200

    Fix permet ech date update on validated propal

--- htdocs/comm/propal/class/propal.class.php
+++ htdocs/comm/propal/class/propal.class.php
@@ -2125,7 +2125,7 @@ class Propal extends CommonObject
 			$this->db->begin();
 
 			$sql = "UPDATE ".MAIN_DB_PREFIX."propal SET fin_validite = ".($date_fin_validite != '' ? "'".$this->db->idate($date_fin_validite)."'" : 'null');
-			$sql .= " WHERE rowid = ".((int) $this->id)." AND fk_statut = ".self::STATUS_DRAFT;
+			$sql .= " WHERE rowid = ".((int) $this->id)." AND fk_statut IN (".self::STATUS_DRAFT.', '.self::STATUS_VALIDATED.')';
 
 			dol_syslog(__METHOD__, LOG_DEBUG);
 			$resql = $this->db->query($sql);
