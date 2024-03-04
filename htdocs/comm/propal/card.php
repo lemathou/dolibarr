6f19987059a810dffda66c880e7e9d1bd9f12664
Mathieu Moulin <mathieu@iprospective.fr>
2024-03-04 19:03:30 +0100

    Fix coquille

--- htdocs/comm/propal/card.php
+++ htdocs/comm/propal/card.php
@@ -2934,7 +2934,6 @@ if ($action == 'create') {
 
 	// Updated by MMI Mathieu Moulin iProspective
 	print '	<form name="addproduct" id="addproduct" action="'.$_SERVER["PHP_SELF"].'?id='.$object->id.(($action != 'editline') ? ($conf->global->MAIN_DOCUMENT_FORM_ADD_PRODUCT_STAY ?'#addproduct' :'') : '#line_'.GETPOST('lineid', 'int')).'" method="POST">
-	//print '	<form name="addproduct" id="addproduct" action="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'" method="POST">
 	<input type="hidden" name="token" value="' . newToken().'">
 	<input type="hidden" name="action" value="' . (($action != 'editline') ? 'addline' : 'updateline').'">
 	<input type="hidden" name="mode" value="">
@@ -2942,6 +2941,7 @@ if ($action == 'create') {
 	<input type="hidden" name="backtopage" value="'.$backtopage.'">
 	<input type="hidden" name="id" value="' . $object->id.'">
 	';
+	//print '	<form name="addproduct" id="addproduct" action="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'" method="POST">
 
 	if (!empty($conf->use_javascript_ajax) && $object->statut == Propal::STATUS_DRAFT) {
 		include DOL_DOCUMENT_ROOT.'/core/tpl/ajaxrow.tpl.php';
